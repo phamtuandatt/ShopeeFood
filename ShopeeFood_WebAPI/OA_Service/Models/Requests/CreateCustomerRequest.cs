36ca4f9f456d0e6423ce4eda858f449d3545fa51
phamtuandatt <109933954+phamtuandatt@users.noreply.github.com>
2023-03-28 10:19:37 +0700

    update

---
+++ ShopeeFood_WebAPI/OA_Service/Models/Requests/CreateCustomerRequest.cs
@@ -0,0 +1,18 @@
+﻿using OA_Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA_Service.Models.Requests
+{
+    public class CreateCustomerRequest
+    {
+        public string CustomerName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+    }
+}
+
---
+++ ShopeeFood_WebAPI/OA_Service/Models/Requests/ResetPasswordRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA_Service.Models.Requests
+{
+    public class ResetPasswordRequest
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
